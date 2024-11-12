a19e9ed9fbf063677c8ea472ed068c8f14c3d28d
xiaoweii <xiaoweii@amazon.com>
2024-11-12 16:01:58 +0800

    ci: update template

--- server/src/main.py
+++ server/src/main.py
@@ -280,9 +280,4 @@ def contains_chinese(text):
 
 if __name__ == "__main__":
     print("Starting webserver...")
-    uvicorn.run(
-        app,
-        host="0.0.0.0",
-        port=int(os.getenv("PORT", 8000)),
-        proxy_headers=True
-    )
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", "8080")))
