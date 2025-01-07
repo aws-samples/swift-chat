971812f0f15818d1ced40c75b0025fb6e324c5ca
xiaoweii <xiaoweii@amazon.com>
2025-01-07 21:10:55 +0800

    feat: add draggable flatlist

--- react-native/src/chat/component/PromptListComponent.tsx
+++ react-native/src/chat/component/PromptListComponent.tsx
@@ -1,12 +1,11 @@
 import React, { useState } from 'react';
 import {
-  StyleSheet,
-  ScrollView,
-  View,
-  TouchableOpacity,
-  Text,
-  Pressable,
   Image,
+  Pressable,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
 } from 'react-native';
 import { MMKV } from 'react-native-mmkv';
 import { useNavigation } from '@react-navigation/native';
@@ -16,6 +15,10 @@ import {
   getSystemPrompts,
   saveSystemPrompts,
 } from '../../storage/StorageUtils.ts';
+import DraggableFlatList, {
+  RenderItemParams,
+  ScaleDecorator,
+} from '@bwjohns4/react-native-draggable-flatlist';
 
 const storage = new MMKV();
 const STORAGE_KEY = 'system_prompts';
@@ -76,66 +79,84 @@ export const PromptListComponent: React.FC<PromptListProps> = ({
     });
   };
 
+  const renderItem = ({
+    item,
+    drag,
+    isActive,
+  }: RenderItemParams<SystemPrompt>) => {
+    return (
+      <ScaleDecorator>
+        <View style={styles.promptContainer}>
+          <Pressable
+            onLongPress={isEditMode ? drag : handleLongPress}
+            onPress={() => handlePromptSelect(item)}
+            style={[
+              styles.promptButton,
+              selectedPrompt?.name === item.name && styles.selectedPromptButton,
+              isActive && styles.draggingPrompt,
+            ]}>
+            <Text
+              style={[
+                styles.promptText,
+                selectedPrompt?.name === item.name && styles.selectedPromptText,
+              ]}>
+              {item.name}
+            </Text>
+          </Pressable>
+          {isEditMode && prompts.length > 1 && (
+            <TouchableOpacity
+              style={styles.deleteTouchable}
+              onPress={() => {
+                const newPrompts = prompts.filter(p => p.name !== item.name);
+                if (selectedPrompt?.name === item.name) {
+                  onSelectPrompt(null);
+                }
+                setPrompts(newPrompts);
+                saveSystemPrompts(newPrompts);
+              }}>
+              <View style={styles.deleteLayout}>
+                <Text style={styles.deleteText}>×</Text>
+              </View>
+            </TouchableOpacity>
+          )}
+        </View>
+      </ScaleDecorator>
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <ScrollView
+      <DraggableFlatList
+        data={prompts}
         horizontal
-        showsHorizontalScrollIndicator={false}
+        renderItem={renderItem}
         keyboardShouldPersistTaps="always"
-        contentContainerStyle={styles.scrollContent}>
-        {prompts.map(prompt => (
-          <View key={prompt.name} style={styles.promptContainer}>
-            <Pressable
-              key={prompt.name}
-              onPress={() => handlePromptSelect(prompt)}
-              onLongPress={handleLongPress}
-              style={[
-                styles.promptButton,
-                selectedPrompt?.name === prompt.name &&
-                  styles.selectedPromptButton,
-              ]}>
-              <Text
-                style={[
-                  styles.promptText,
-                  selectedPrompt?.name === prompt.name &&
-                    styles.selectedPromptText,
-                ]}>
-                {prompt.name}
-              </Text>
-            </Pressable>
-            {isEditMode && prompts.length > 1 && (
-              <TouchableOpacity
-                style={styles.deleteTouchable}
-                onPress={() => {
-                  const newPrompts = prompts.filter(
-                    p => p.name !== prompt.name
-                  );
-                  if (selectedPrompt?.name === prompt.name) {
-                    onSelectPrompt(null);
-                  }
-                  setPrompts(newPrompts);
-                  saveSystemPrompts(newPrompts);
-                }}>
-                <View style={styles.deleteLayout}>
-                  <Text style={styles.deleteText}>×</Text>
-                </View>
-              </TouchableOpacity>
-            )}
-          </View>
-        ))}
-        {isEditMode && (
-          <TouchableOpacity style={styles.addButton} onPress={handleAddPrompt}>
-            <Text style={styles.addText}>+</Text>
-          </TouchableOpacity>
-        )}
-      </ScrollView>
+        alwaysBounceHorizontal={true}
+        keyExtractor={item => item.name}
+        onDragEnd={({ data }) => {
+          setPrompts(data);
+          saveSystemPrompts(data);
+        }}
+        containerStyle={styles.scrollContent}
+        showsHorizontalScrollIndicator={false}
+        ListFooterComponent={
+          isEditMode ? (
+            <TouchableOpacity
+              style={styles.addButton}
+              onPress={handleAddPrompt}>
+              <Text style={styles.addText}>+</Text>
+            </TouchableOpacity>
+          ) : null
+        }
+      />
       {isEditMode && (
         <TouchableOpacity
           style={styles.doneButton}
           onPress={() => setIsEditMode(false)}>
           <Image
             source={require('../../assets/done.png')}
-            style={styles.doneImage}></Image>
+            style={styles.doneImage}
+          />
         </TouchableOpacity>
       )}
     </View>
@@ -144,20 +165,21 @@ export const PromptListComponent: React.FC<PromptListProps> = ({
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     height: 60,
     flexDirection: 'row',
     alignItems: 'center',
   },
   scrollContent: {
-    paddingHorizontal: 12,
+    flex: 1,
   },
   promptButton: {
     height: 36,
-    paddingHorizontal: 12,
+    paddingHorizontal: 10,
     paddingVertical: 8,
     backgroundColor: '#E8E8E8',
     borderRadius: 8,
-    marginRight: 8,
+    marginLeft: 8,
     borderWidth: 1.2,
     borderColor: '#E8E8E8',
   },
@@ -176,7 +198,8 @@ const styles = StyleSheet.create({
     width: 32,
     height: 32,
     borderRadius: 20,
-    marginTop: 7,
+    marginTop: 17,
+    marginLeft: 8,
     backgroundColor: '#fff',
     justifyContent: 'center',
     alignItems: 'center',
@@ -189,20 +212,21 @@ const styles = StyleSheet.create({
     marginTop: -1.5,
   },
   doneButton: {
-    padding: 20,
-    marginTop: 6,
+    paddingHorizontal: 20,
+    paddingTop: 24,
+    paddingBottom: 16,
   },
   doneImage: {
     width: 20,
     height: 20,
   },
   promptContainer: {
-    position: 'relative',
+    alignSelf: 'center',
     marginTop: 6,
   },
   deleteTouchable: {
     position: 'absolute',
-    right: -0,
+    right: -8,
     top: -8,
     zIndex: 1,
     width: 24,
@@ -224,5 +248,8 @@ const styles = StyleSheet.create({
     marginTop: -2,
     marginRight: -0.5,
     fontWeight: 'normal',
+  },
+  draggingPrompt: {
+    opacity: 0.9,
   },
 });
