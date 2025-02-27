b59070302e10dbcc2c3108b33a151573f8c9c4e1
xiaoweii <xiaoweii@amazon.com>
2025-02-27 09:09:32 +0800

    feat: add flexDirection to column-reverse to support correct reasoning expanded direction

--- react-native/src/chat/ChatScreen.tsx
+++ react-native/src/chat/ChatScreen.tsx
@@ -358,12 +358,12 @@ function ChatScreen(): React.JSX.Element {
     },
   });
 
-  const scrollToTop = () => {
+  const scrollToBottom = () => {
     if (flatListRef.current) {
       flatListRef.current.scrollToEnd();
     }
   };
-  const scrollToBottom = () => {
+  const scrollToTop = () => {
     if (flatListRef.current) {
       flatListRef.current.scrollToOffset({ offset: 0, animated: true });
     }
@@ -514,7 +514,7 @@ function ChatScreen(): React.JSX.Element {
           _id: 1,
         }}
         alignTop={false}
-        inverted={true}
+        inverted={false}
         renderChatEmpty={() => (
           <EmptyChatComponent chatMode={modeRef.current} />
         )}
@@ -612,6 +612,7 @@ const styles = StyleSheet.create({
     paddingBottom: 15,
     flexGrow: 1,
     justifyContent: 'flex-end',
+    flexDirection: 'column-reverse',
   },
   textInputStyle: {
     marginLeft: 14,
